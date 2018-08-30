b5a39f25a6437ecab11b7376ad6b14b33861799d
Max <ultiwd@gmail.com>
2018-08-30 12:03:03 +0300

    fix

---
+++ dist/precache-manifest.956cfdfafc39ecd5b0634027cec1e319.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0eaa66aec9c70667468e",
+    "url": "/js/chunk-vendors.dd398368.js"
+  },
+  {
+    "revision": "c12f09e211962ac93961",
+    "url": "/js/app.ede26f7b.js"
+  },
+  {
+    "revision": "24eba51765fd3fdd26c4",
+    "url": "/js/about.fd1060eb.js"
+  },
+  {
+    "revision": "4d07ffc51017109073b84738934b07c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c12f09e211962ac93961",
+    "url": "/css/app.822b485e.css"
+  },
+  {
+    "revision": "24eba51765fd3fdd26c4",
+    "url": "/css/about.69d9b447.css"
+  }
+];
