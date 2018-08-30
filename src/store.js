3add98c5973a4b9e64dd4da994057497b64f4aaf
Max <ultiwd@gmail.com>
2018-08-30 11:48:27 +0300

    For vano

--- src/store.js
+++ src/store.js
@@ -5,12 +5,56 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    count: 0,
+    numbers: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+    ],
+    operands: [
+      '+', '-', '*', '/',
+    ],
+    num: '',
+    op: '',
+    totals: null,
+  },
+  getters: {
+    COUNT: state => state.count,
+    NUMBERS: state => state.numbers,
+    OPERANDS: state => state.operands,
+    CURRENT_OPERAND: state => state.op,
+    CURRENT_NUM: state => state.num,
+    TOTALS: state => state.totals,
   },
   mutations: {
-
+    CURRENT_NUMBER: (state, payload) => state.num = payload,
+    CURRENT_OPERAND: (state, payload) => state.op = payload,
+    CALC: (state, payload) => {
+      switch (state.op) {
+        case '+':
+          if (state.totals || state.totals === 0) { return state.totals += payload; }
+          return state.totals = state.num + payload;
+        case '-':
+          if (state.totals || state.totals === 0) { return state.totals -= payload; }
+          return state.totals = state.num - payload;
+        case '*':
+          if (state.totals || state.totals === 0) { return state.totals *= payload; }
+          return state.totals = state.num * payload;
+        case '/':
+          if (state.totals || state.totals === 0) { return state.totals /= payload; }
+          return state.totals = state.num / payload;
+        default:
+          return state;
+      }
+    },
   },
   actions: {
-
+    SET_CURRENT_NUBMER: (context, payload) => {
+      context.commit('CURRENT_NUMBER', payload);
+    },
+    SET_CURRENT_OPERAND: (context, payload) => {
+      context.commit('CURRENT_OPERAND', payload);
+    },
+    CALC: (context, payload) => {
+      context.commit('CALC', payload);
+    },
   },
 });
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,102 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="result">{{animatedNumber === false ? '' : animatedNumber}}</div>
+    <div class="grid num">
+      <div v-for="num in numbers" @click="setCurrentNumber(num)">{{num}}</div>
+    </div>
+    <div class="grid">
+      <div v-for="op in operands" @click="setCurrentOperand(op)"><span>{{op}}</span></div>
+    </div>
+    <!-- <span @click="decrease">-</span> -->
+    <!-- <h1>{{computedCount}}</h1> -->
+    <!-- <span @click="increase">+</span> -->
   </div>
 </template>
+
+<script>
+import {TweenMax, Power2, TimelineLite} from "gsap/TweenMax";
+
+export default {
+  data(){
+    return {
+    count: 0,
+    totals: '',
+    tweenedNumber: 0
+    }
+  },
+  methods: {
+    setCurrentNumber(number) {
+      if (this.$store.getters.CURRENT_OPERAND 
+      &&  this.$store.getters.CURRENT_NUM) {
+        return this.calc(number)
+      }
+      return this.$store.dispatch('SET_CURRENT_NUBMER', number)
+    },
+    setCurrentOperand(operand) {
+      this.$store.dispatch('SET_CURRENT_OPERAND', operand)
+    },
+    calc(number) {
+      this.$store.dispatch('CALC', number)
+    }
+  },
+  computed: {
+    newTotals() {
+      return this.$store.getters.TOTALS
+    },
+    animatedNumber: function() {
+      return this.newTotals !== null && this.tweenedNumber.toFixed(0);
+    },
+    computedCount: {
+      get() {
+        return this.count = this.$store.getters.COUNT
+      }
+    },
+    operands: {
+      get() {
+        return this.operands = this.$store.getters.OPERANDS
+      }
+    },
+    numbers: {
+      get() {
+        return this.numbers = this.$store.getters.NUMBERS
+      }
+    }
+  },
+  watch: {
+    newTotals: function(newValue) {
+      TweenLite.to(this.$data, 0.5, { tweenedNumber: newValue });
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    width: 250px;
+    margin: auto;
+    align-items: center;
+    div {
+      font-size: 40px;
+      font-weight: bold;
+      box-shadow: 0px 0px 0.02px #00000040;
+      cursor: pointer;
+      transition: box-shadow 0.3s linear;
+    }
+    div:hover {
+        box-shadow: 0px 0px 4px 0px #0096ffd4;
+      }
+
+
+    & :last-child {
+        grid-column: auto / span 3;
+    }
+  }
+
+  .result {
+    font-size: 40px;
+    height: 45px;
+    font-weight: bold;
+  }
+</style>
